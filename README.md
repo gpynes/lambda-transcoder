cd14767060a080e5d7ba8c8ff04c269421aa7b88
Greg Pynes <gpynes@gmail.com>
2020-04-17 21:51:08 -0700

    feat(Big Commit): gets lifecycle working + adds docs

--- README.md
+++ README.md
@@ -12,3 +12,25 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
  * `cdk deploy`      deploy this stack to your default AWS account/region
  * `cdk diff`        compare deployed stack with current state
  * `cdk synth`       emits the synthesized CloudFormation template
+
+
+## Getting Started
+
+To get started first initialize the cdk using `cdk bootstrap`. Then deploy this app using `npm run build` followed by `cdk deploy`.
+
+Once the app is deployed, simply upload a video file to the `source/` directory inside the `SourceBucket` that is created to trigger the pipeline (you will need to manually create the `source/` directory).
+
+This is still a work in progress so it doesn't work on big videos just yet, I recommend a ~60mb `.mov` file as that's what I was testing with.
+
+## How it works
+
+1. When a file is uploaded to the `SourceBucket`'s `source/` directory a lambda called `OnUploadInitTranscodeHandler` is triggered.
+2. `OnUploadInitTranscodeHandler` takes the `Key` and `Bucket` of the uploaded file and kicks off a `TranscodeMachine` execution.
+3. The `TranscodeMachine` consists of:
+   1. A `ProbeTask` that reads the file and creates a list of **start+end** times so we can parallelize the transcoding in 5s chunks.
+   2. A `TranscodeParts` task that takes the list of **start+end** times and triggers an `TranscodePartTask` for each 5s chunk to process in parallel.
+   3. An `AssemblePartsTask` that takes all the chunks produced by the `TranscodePartTask`s and concats their outputs into a single file in the `output/` directory of the `SourceBucket`.
+   4. Finally a `CleanUpTask` is fired to clean up all the temporary chunks created by the `TranscodePartTask`s.
+
+
+![step_functions_diagram](./stepfunctions_graph.png)
--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,8 +1,11 @@
 import { Stack, Construct, Duration } from '@aws-cdk/core'
 import { Map, StateMachine, Task } from '@aws-cdk/aws-stepfunctions'
 import { InvokeFunction } from '@aws-cdk/aws-stepfunctions-tasks'
-import { Bucket } from '@aws-cdk/aws-s3'
+import { Bucket, EventType } from '@aws-cdk/aws-s3'
+import { S3EventSource } from '@aws-cdk/aws-lambda-event-sources'
 import { TranscodeLambda } from './TranscodeLambda'
+import { S3CreateEvent } from 'aws-lambda'
+import { InlineCode, Function, Runtime } from '@aws-cdk/aws-lambda'
 
 export class ElasticTranscode extends Stack {
   constructor(scope: Construct, id: string) {
@@ -22,7 +25,7 @@ export class ElasticTranscode extends Stack {
     const assemblerHandler = new TranscodeLambda(this, 'AssemblerHandler', {
       handler: 'assemblerHandler',
       timeout: Duration.minutes(3),
-      memorySize: 2048
+      memorySize: 3008
     })
     const cleanupHandler = new TranscodeLambda(this, 'CleanUpHandler', {
       handler: 'cleanupHandler',
@@ -42,24 +45,73 @@ export class ElasticTranscode extends Stack {
     const probeTask = new Task(this, 'ProbeTask', {
       task: new InvokeFunction(probeHandler),
     })
-    const assemblePartsTask = new Task(this, 'AssemblePartsTask', {
-      task: new InvokeFunction(assemblerHandler),
-    })
+
     const transcodePartTask = new Task(this, 'TranscodePartTask', {
       task: new InvokeFunction(transcodePartHandler),
+    })
+    const transcodePartsTask = new Map(this, 'TranscodeParts')
+    transcodePartsTask.iterator(transcodePartTask)
+    
+    const assemblePartsTask = new Task(this, 'AssemblePartsTask', {
+      task: new InvokeFunction(assemblerHandler),
     })
     const cleanUpTask = new Task(this, 'CleanUpTask', {
       task: new InvokeFunction(cleanupHandler),
     })
     
-    
     // Step Function State Machine
-    const transcodePartsTask = new Map(this, 'TranscodeParts')
-    transcodePartsTask.iterator(transcodePartTask)
     const definition = probeTask.next(transcodePartsTask).next(assemblePartsTask).next(cleanUpTask)
-    const stateMachine = new StateMachine(this, 'ElasticTranscodeMachine', {
+    const stateMachine = new StateMachine(this, 'TranscodeMachine', {
       definition,
       timeout: Duration.minutes(3),
     })
+
+    // S3 Bucket Upload Trigger
+    const onUploadEvent = new S3EventSource(sourceBucket, {
+      events: [
+        EventType.OBJECT_CREATED,
+      ],
+      filters: [{
+        prefix: 'source/'
+      }]
+    })
+
+    const onUploadInitTranscode = new Function(this, 'OnUploadInitTranscodeHandler', {
+      code: new InlineCode(`
+        module.exports.handler = ${onUploadInitTranscodeHandler.toString()
+          // Replace the code string of the statemachine arn with the actual arn
+          .replace('stateMachine.stateMachineArn', `'${stateMachine.stateMachineArn}'`)}
+      `),
+      handler: 'index.handler',
+      runtime: Runtime.NODEJS_12_X,
+      timeout: Duration.seconds(10)
+    })
+    // Allow Init to start the step function
+    stateMachine.grantStartExecution(onUploadInitTranscode)
+    
+    // Connect the s3 lifecycle event to transcode init handler
+    onUploadInitTranscode.addEventSource(onUploadEvent)
+
+    // Handler Definition
+    async function onUploadInitTranscodeHandler(event: S3CreateEvent) {
+      try {
+        const { StepFunctions } = require('aws-sdk')
+        const step = new StepFunctions()
+        const { bucket, object } = event.Records[0].s3
+        const Bucket = bucket.name
+        const Key = object.key
+        const stateMachineArn = stateMachine.stateMachineArn
+        
+        console.log('Initializing', stateMachineArn)
+        console.log('WITH', Bucket, Key)
+        
+        return step.startExecution({
+          stateMachineArn,
+          input: JSON.stringify({ Bucket, Key })
+        }).promise()
+      } catch(err) {
+        console.log('Error', err)
+      }
+    }
   }
 }
